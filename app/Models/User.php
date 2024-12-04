2abf099d09812cd1f333e54c77e7e3439a4b3e50
Parizetm <151616242+ParizetM@users.noreply.github.com>
2024-12-04 14:24:23 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
 
 class User extends Authenticatable
 {
+    use HasRolesAndAbilities;
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
