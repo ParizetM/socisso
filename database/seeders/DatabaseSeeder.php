1014c8e7a3a4ecf4852df89092f1788b58814878
Parizetm <151616242+ParizetM@users.noreply.github.com>
2024-12-04 15:10:53 +0100

    nom prenom

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'nom' => 'ADMIN',
+            'prenom' => 'admin',
+            'email' => 'admin@socisso.fr',
+            'password' => Hash::make('Socisso$2024$Admin')
         ]);
     }
 }
